ca5bff7ba6852e8d02becdf1be713df9115fe5cd
Eric Johnson <ericartjohnson@gmail.com>
2013-01-14 16:15:12 -0500

    cleanup

--- Assets/Classes/Cameras/CameraFollow.cs
+++ Assets/Classes/Cameras/CameraFollow.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class CameraFollow : MonoBehaviour {
 
 	public Transform targetTransform;
+	public CharacterController controller;
 
 	// Use this for initialization
 	void Start () {
@@ -12,6 +13,8 @@ public class CameraFollow : MonoBehaviour {
 	
 	// LateUpdate is called once per frame after all other Update calls
 	void LateUpdate () {
+		Debug.Log(controller.velocity);
+
 		transform.position = new Vector3(targetTransform.position.x,targetTransform.position.y,transform.position.z);
 	}
 }
